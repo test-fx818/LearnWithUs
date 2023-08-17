6147db3dedf13758e28cd62f3bdc438e9ad593a4
Anurag Upadhyay <115972402+fx818@users.noreply.github.com>
2023-08-18 00:19:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const downloadRef = document.getElementsByClassName('download-button')[0];
+// downloadRef = document.getElementsByClassName('download-button2')[0];
+
+
+function download(){
+    downloadRef.innerHTML = 'Downloading';
+
+}
